828c962b34ee0effd85d87581432e1adf32d11a8
jasurbek <jasurbekjumaboyev57@gmail.com>
2020-12-15 18:59:46 +0500

    test

--- degree/degree.js
+++ degree/degree.js
@@ -3,15 +3,16 @@
     
     let l =  +prompt('please enter the its level')
     
-    let answer
+    let answer = 1
     
     if (l == 0) {
         alert(answer + 'the response rate cannot be 0')
     }else if (number <= 0) {
         alert('such a number cannot be entered')
     }else {
-        for (answer = number; l > 1; ) {
+        for (let i = 1; i <= l; i++) {
             answer = answer * number
+            console.log(i);
         }
         
     }
